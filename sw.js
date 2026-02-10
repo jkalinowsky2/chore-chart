255054b2c750e25f4be21be8cd51130c17ff2bb6
JK <jkalinowsky@gmail.com>
2026-02-10 12:47:34 -0500

    Initial commit of chore chart

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "switch-chore-chart-v1";
+
+// If you later add more files, add them here
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
